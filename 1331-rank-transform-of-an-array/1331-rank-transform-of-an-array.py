37d006e3c5e688469b51a5ab85e9247335663ed8
Boyalla Sumanth Reddy <oksumanth@gmail.com>
2026-07-12 23:57:17 +0530

    Time: 33 ms (82.61%), Space: 37.4 MB (80.09%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def arrayRankTransform(self, arr: List[int]) -> List[int]:
+        rank = {}
+        for i, num in enumerate(sorted(set(arr)), 1):
+            rank[num] = i
+        return [rank[num] for num in arr]
