cc83f78366ceb6724f274faa1ece89e28b9885e7
Boyalla Sumanth Reddy <oksumanth@gmail.com>
2026-08-24 21:53:45 +0530

    Time: 674 ms (65.09%), Space: 33.1 MB (63.91%) - LeetHub

---
+++ 1872-stone-game-viii/1872-stone-game-viii.py
@@ -0,0 +1,16 @@
+class Solution:
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+
+        prefix = [0] * n
+        prefix[0] = stones[0]
+
+        for i in range(1, n):
+            prefix[i] = prefix[i - 1] + stones[i]
+
+        ans = prefix[-1]
+
+        for i in range(n - 2, 0, -1):
+            ans = max(ans, prefix[i] - ans)
+
+        return ans
