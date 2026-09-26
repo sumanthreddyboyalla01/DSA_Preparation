435d8e5cd2f9053e2314c670289d022b5fa66d6b
Boyalla Sumanth Reddy <oksumanth@gmail.com>
2026-09-26 18:35:44 +0530

    Time: 43 ms (82.55%), Space: 51.3 MB (81.62%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.py
@@ -1,12 +1,6 @@
+import re
+
 class Solution:
     def evaluate(self, s: str, knowledge: list[list[str]]) -> str:
         d = dict(knowledge)
-        parts = s.split('(')
-        
-        res = [parts[0]]
-        for part in parts[1:]:
-            key, rest = part.split(')')
-            res.append(d.get(key, "?"))
-            res.append(rest)
-            
-        return "".join(res)
+        return re.sub(r'\((.*?)\)', lambda m: d.get(m.group(1), '?'), s)
